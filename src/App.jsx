18646f3f016ae3de5e9008d636ca7fd63ef869f6
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2024-10-31 13:57:06 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import UsersList from './UsersList.jsx';
+import UsersDetails from './UsersDetails.jsx';
+
+const App = () => {
+  const [showList, setShowList] = useState(true);
+  const [userDetails, setUserDetails] = useState({});
+  
+  return (
+    <>
+      <h1>Billionaire Hackers</h1>
+      
+      {
+        showList ? 
+          <UsersList 
+            setShowList={setShowList} 
+            setUserDetails={setUserDetails} 
+          /> : 
+          <UsersDetails 
+            userDetails={userDetails}
+            setShowList={setShowList}
+          />
+      }
+    </>
+  )
+}
+
+export default App
